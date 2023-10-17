8fdb517cffd0aa55ede2764fd8d303ea48cafba9
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-10-16 20:36:28 -0600

    Complete: get one merchant endpoint

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -20,4 +20,20 @@ RSpec.describe "Merchants API" do
       expect(merchant[:name]).to be_a(String)
     end
   end
+
+  it "should get one merchant by id" do 
+    id = create(:merchant).id
+  
+    get "/api/v1/merchants/#{id}"
+  
+    merchant = JSON.parse(response.body, symbolize_names: true)
+  
+    expect(response).to be_successful
+  
+    expect(merchant).to have_key(:id)
+    expect(merchant[:id]).to eq(id)
+  
+    expect(merchant).to have_key(:name)
+    expect(merchant[:name]).to be_a(String)
+  end
 end
