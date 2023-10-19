51ac0eba12cc6f5bf2b48d8b19374690b1a4318e
Ethan Bustamante <ethanjamesb2004@gmail.com>
2023-10-19 11:41:16 -0600

    Follow MVC

--- app/controllers/api/v1/merchants/search_controller.rb
+++ app/controllers/api/v1/merchants/search_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::Merchants::SearchController < ApplicationController
   def find
-    merchant = Merchant.where("LOWER(name) LIKE ?", "%#{params[:name].downcase}%").order(:name).first
+    if params[:name]
+      merchant = Merchant.search_by_name(params[:name])
 
-    if merchant
-      render json: MerchantSerializer.new(merchant)
-    else
-      render json: { data: { attributes: [] } }
+      if merchant
+        render json: MerchantSerializer.new(merchant)
+      else
+        render json: { data: { attributes: [] } }
+      end
     end
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,3 +1,7 @@
 class Merchant < ApplicationRecord
   has_many :items
+
+  def self.search_by_name(name)
+    Merchant.where("LOWER(name) LIKE ?", "%#{name.downcase}%").order(:name).first
+  end
 end
